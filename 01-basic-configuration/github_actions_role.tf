41f609219a2b2d5f9fd7b35b56c0fa8c915a6e26
tolebi.yermek <tolebiermekov@gmail.com>
2025-06-23 00:31:01 +0500

    hot fix: removed OIDC provider

--- 01-basic-configuration/github_actions_role.tf
+++ 01-basic-configuration/github_actions_role.tf
@@ -1,9 +1,13 @@
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
 data "aws_iam_policy_document" "github_actions_trust" {
   statement {
     effect = "Allow"
     principals {
       type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github.arn]
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
     }
     actions = ["sts:AssumeRoleWithWebIdentity"]
     condition {
@@ -17,22 +21,3 @@ data "aws_iam_policy_document" "github_actions_trust" {
 }
 
 data "aws_caller_identity" "current" {}
-
-resource "aws_iam_role" "github_actions" {
-  name               = "GithubActionsRole"
-  assume_role_policy = data.aws_iam_policy_document.github_actions_trust.json
-}
-
-resource "aws_iam_role_policy_attachment" "github_actions_attach" {
-  for_each = toset([
-    "AmazonEC2FullAccess",
-    "AmazonRoute53FullAccess",
-    "AmazonS3FullAccess",
-    "IAMFullAccess",
-    "AmazonVPCFullAccess",
-    "AmazonSQSFullAccess",
-    "AmazonEventBridgeFullAccess"
-  ])
-  role       = aws_iam_role.github_actions.name
-  policy_arn = "arn:aws:iam::aws:policy/${each.value}"
-}
